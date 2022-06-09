1894f8c2b669f84d13f195138f318b21bfc2af73
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2022-06-09 02:57:29 +0100

    Add files via upload

--- GryllsExpBar.lua
+++ GryllsExpBar.lua
@@ -383,12 +383,12 @@ local function GryllsExpBar_commands(msg, editbox)
         DEFAULT_CHAT_FRAME:AddMessage("|c"..orange.."Grylls|rExpBar: exp bar has been reset")
     else
         DEFAULT_CHAT_FRAME:AddMessage("|c"..orange.."Grylls|rExpBar usage:")
+        DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb move |r - toggles allowing moving of the bar by dragging it")   
         DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb width n|r - set bar to width n")
         DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb height n|r - set bar to height n")
         DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb border |r - toggle bar border")
         DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb dark |r - toggle dark theme border")
-        DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb reset |r - reset bar to default settings")
-        DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb move |r - toggles allowing moving of the bar by dragging it")        
+        DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb reset |r - reset bar to default settings")             
     end
 end
 
