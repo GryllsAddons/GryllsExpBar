f1e4f62615d8dd374521e2e12ee82e23053100ce
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2022-06-10 02:29:06 +0100

    Add files via upload

--- GryllsExpBar.lua
+++ GryllsExpBar.lua
@@ -3,7 +3,7 @@ local GryllsExpBar = CreateFrame("Frame", nil, UIParent)
 
 GryllsExpBar_Settings = {
     barWidth = 465,
-    barHeight = 18,    
+    barHeight = 18,
     barLeft = 1075,
     barTop = 200,
     barBorder = true,
@@ -22,38 +22,38 @@ local function roundnum(input, places)
 end
 
 local function createBar()
-    -- local barTexture = "Interface/Tooltips/UI-Tooltip-Background"    
+    -- local barTexture = "Interface/Tooltips/UI-Tooltip-Background"
     local barTexture = "Interface/TargetingFrame/UI-StatusBar"
 
     -- (base layer to anchor other bars to)
     GryllsExpBar.backgroundBar = CreateFrame("StatusBar", nil, UIParent)
     GryllsExpBar.backgroundBar:SetFrameStrata("BACKGROUND")
-    GryllsExpBar.backgroundBar:SetFrameLevel(0)  
+    GryllsExpBar.backgroundBar:SetFrameLevel(0)
     GryllsExpBar.backgroundBar:SetMovable(true)
-    GryllsExpBar.backgroundBar:SetClampedToScreen()    
+    GryllsExpBar.backgroundBar:SetClampedToScreen()
 
     GryllsExpBar.backgroundBar.bg = GryllsExpBar.backgroundBar:CreateTexture(nil, "BACKGROUND")
     GryllsExpBar.backgroundBar.bg:SetTexture(barTexture)
     GryllsExpBar.backgroundBar.bg:SetAllPoints(true)
-    GryllsExpBar.backgroundBar.bg:SetVertexColor(0, 0, 0, 0.5)    
-    
-    GryllsExpBar.restedBar = CreateFrame("StatusBar", nil, GryllsExpBar.backgroundBar)       
-    GryllsExpBar.restedBar:SetStatusBarTexture(barTexture)    
+    GryllsExpBar.backgroundBar.bg:SetVertexColor(0, 0, 0, 0.5)
+
+    GryllsExpBar.restedBar = CreateFrame("StatusBar", nil, GryllsExpBar.backgroundBar)
+    GryllsExpBar.restedBar:SetStatusBarTexture(barTexture)
     GryllsExpBar.restedBar:SetFrameStrata("BACKGROUND")
     GryllsExpBar.restedBar:SetFrameLevel(1)
     GryllsExpBar.restedBar:SetAllPoints(GryllsExpBar.backgroundBar)
 
-    GryllsExpBar.expBar = CreateFrame("StatusBar", nil, GryllsExpBar.backgroundBar)    
+    GryllsExpBar.expBar = CreateFrame("StatusBar", nil, GryllsExpBar.backgroundBar)
     GryllsExpBar.expBar:SetStatusBarTexture(barTexture)
     GryllsExpBar.expBar:SetFrameStrata("BACKGROUND")
     GryllsExpBar.expBar:SetFrameLevel(2)
-    GryllsExpBar.expBar:SetAllPoints(GryllsExpBar.backgroundBar)    
+    GryllsExpBar.expBar:SetAllPoints(GryllsExpBar.backgroundBar)
 
-    GryllsExpBar.repBar = CreateFrame("StatusBar", nil, GryllsExpBar.backgroundBar)       
+    GryllsExpBar.repBar = CreateFrame("StatusBar", nil, GryllsExpBar.backgroundBar)
     GryllsExpBar.repBar:SetStatusBarTexture(barTexture)
     GryllsExpBar.repBar:SetFrameStrata("BACKGROUND")
     GryllsExpBar.repBar:SetFrameLevel(3)
-    GryllsExpBar.repBar:SetAllPoints(GryllsExpBar.backgroundBar)    
+    GryllsExpBar.repBar:SetAllPoints(GryllsExpBar.backgroundBar)
 
     GryllsExpBar.border = CreateFrame("Frame", nil, GryllsExpBar.backgroundBar)
     local p = 6
@@ -64,29 +64,29 @@ local function createBar()
 
     local e, i = 19, 0
     GryllsExpBar.border:SetBackdrop(
-        {        
+        {
             bgFile = "Interface/TargetingFrame/UI-StatusBar",
             edgeFile = "Interface/DialogFrame/UI-DialogBox-Border",
-            tile = true, edgeSize = e, 
+            tile = true, edgeSize = e,
             insets = { left = i, right = i, top = i, bottom = i }
         }
-    )        
+    )
     GryllsExpBar.border:SetBackdropBorderColor(1, 1, 1, 1)
     GryllsExpBar.border:SetBackdropColor(1,1,1,0)
     GryllsExpBar.string = CreateFrame("Frame", nil, GryllsExpBar.backgroundBar)
-    GryllsExpBar.string:SetAllPoints(GryllsExpBar.backgroundBar)    
+    GryllsExpBar.string:SetAllPoints(GryllsExpBar.backgroundBar)
     GryllsExpBar.string:SetFrameStrata("BACKGROUND")
     GryllsExpBar.string:SetFrameLevel(5)
 
-    local font, size, outline = "Fonts\\frizqt__.TTF", 12, "OUTLINE"    
+    local font, size, outline = "Fonts\\frizqt__.TTF", 12, "OUTLINE"
     GryllsExpBar.string.expText = GryllsExpBar.string:CreateFontString(nil, "OVERLAY", "GameFontWhite")
     GryllsExpBar.string.expText:SetFont(font, size, outline)
-    GryllsExpBar.string.expText:SetPoint("CENTER", GryllsExpBar.string, "CENTER", 0, 1)    
+    GryllsExpBar.string.expText:SetPoint("CENTER", GryllsExpBar.string, "CENTER", 0, 1)
     GryllsExpBar.string.expText:Hide()
 
     GryllsExpBar.string.repText = GryllsExpBar.string:CreateFontString(nil, "OVERLAY", "GameFontWhite")
     GryllsExpBar.string.repText:SetFont(font, size, outline)
-    GryllsExpBar.string.repText:SetAllPoints(GryllsExpBar.string.expText)    
+    GryllsExpBar.string.repText:SetAllPoints(GryllsExpBar.string.expText)
     GryllsExpBar.string.repText:Hide()
 
     GryllsExpBar.mouse = CreateFrame("Frame", nil, GryllsExpBar.backgroundBar)
@@ -95,14 +95,14 @@ local function createBar()
     GryllsExpBar.mouse:SetFrameLevel(5)
     GryllsExpBar.mouse:EnableMouse(true)
     GryllsExpBar.mouse:RegisterForDrag("LeftButton")
-    
+
     GryllsExpBar.mouse:SetScript("OnEnter", function()
         local name = GetWatchedFactionInfo()
         if name then
             GryllsExpBar.string.expText:Hide()
             GryllsExpBar.string.repText:Show()
         else
-            GryllsExpBar.string.repText:Hide()     
+            GryllsExpBar.string.repText:Hide()
             GryllsExpBar.string.expText:Show()
         end
     end)
@@ -110,14 +110,14 @@ local function createBar()
     GryllsExpBar.mouse:SetScript("OnLeave", function()
         GryllsExpBar.string.repText:Hide()
         GryllsExpBar.string.expText:Hide()
-    end)    
+    end)
 
     GryllsExpBar.mouse:SetScript("OnDragStart", function()
         if GryllsExpBar.move then
             GryllsExpBar.backgroundBar:StartMoving()
         end
     end)
-    
+
     GryllsExpBar.mouse:SetScript("OnDragStop", function()
         GryllsExpBar.backgroundBar:StopMovingOrSizing()
         GryllsExpBar_Settings.barLeft = GryllsExpBar.backgroundBar:GetLeft()
@@ -129,21 +129,21 @@ end
 local function updateExp()
     local playerlevel = UnitLevel("player")
     -- credit to Shagu (https://shagu.org/pfUI/) for code below
-    local function ExpText(xp, xpmax, exh, xp_perc, remaining, remaining_perc, playerlevel)    
+    local function ExpText(xp, xpmax, exh, xp_perc, remaining, remaining_perc, playerlevel)
         if playerlevel < 60 then
-            if exh ~= 0 then        
+            if exh ~= 0 then
                 local exh_perc = roundnum(exh / xpmax * 100) or 0
                 return "Level "..playerlevel.." - "..remaining.." ("..remaining_perc.."%) remaining - "..exh.." ("..exh_perc.."%) rested"
             else
                 return "Level "..playerlevel.." - "..remaining.." ("..remaining_perc.."%) remaining"
             end
-        end 
-    end    
+        end
+    end
 
     local xp, xpmax, exh = UnitXP("player"), UnitXPMax("player"), GetXPExhaustion() or 0
     local xp_perc = roundnum(xp / xpmax * 100)
     local remaining = xpmax - xp
-    local remaining_perc = roundnum(remaining / xpmax * 100)    
+    local remaining_perc = roundnum(remaining / xpmax * 100)
 
     -- set values for expBar
     GryllsExpBar.expBar:SetMinMaxValues(0, xpmax)
@@ -153,7 +153,7 @@ local function updateExp()
 
     -- as you can rest into the next level...
     if exh > remaining then
-        GryllsExpBar.restedBar:SetValue(xpmax) -- fill the restedBar as we have rested into the next level 
+        GryllsExpBar.restedBar:SetValue(xpmax) -- fill the restedBar as we have rested into the next level
     else
         GryllsExpBar.restedBar:SetValue(xp + exh)
     end
@@ -170,12 +170,12 @@ local function updateExp()
         else
             r,g,b = 0.6, 0, 0.6
         end
-    end 
-    
+    end
+
     GryllsExpBar.expBar:SetStatusBarColor(r,g,b,1)
     GryllsExpBar.restedBar:SetStatusBarColor(0, 0.5, 1, 0.5)
 
-    -- set exp text       
+    -- set exp text
     GryllsExpBar.string.expText:SetText(ExpText(xp, xpmax, exh, xp_perc, remaining, remaining_perc, playerlevel))
 end
 
@@ -200,17 +200,17 @@ local function updateRep()
 
         -- set rep text
         GryllsExpBar.string.repText:SetText(name .. " (" .. repvalues[standing] .. ") " .. percentFloor .. "% - "  .. roundnum(remaining) .. " remaining")
-        
+
         -- set rep colors
         local r = FACTION_BAR_COLORS[standing].r;
         local g = FACTION_BAR_COLORS[standing].g;
         local b = FACTION_BAR_COLORS[standing].b;
         GryllsExpBar.repBar:SetStatusBarColor(r,g,b,1)
         --GryllsExpBar.string.repText:Show()
-    else 
+    else
         -- not watching a faction
         GryllsExpBar.repBar:Hide()
-        updateExp(frame)        
+        updateExp(frame)
         --GryllsExpBar.string.expText:Show()
     end
 end
@@ -274,7 +274,7 @@ local function GryllsExpBar_commands(msg, editbox)
         num = fontnum(msg)
         GryllsExpBar_Settings.barHeight = num
         setBar()
-        DEFAULT_CHAT_FRAME:AddMessage("|c"..orange.."Grylls|rExpBar: height set to "..num)    
+        DEFAULT_CHAT_FRAME:AddMessage("|c"..orange.."Grylls|rExpBar: height set to "..num)
     elseif msg == "border" then
         if GryllsExpBar_Settings.barBorder then
             GryllsExpBar_Settings.barBorder = false
@@ -316,13 +316,13 @@ local function GryllsExpBar_commands(msg, editbox)
         DEFAULT_CHAT_FRAME:AddMessage("|c"..orange.."Grylls|rExpBar: exp bar has been reset")
     else
         DEFAULT_CHAT_FRAME:AddMessage("|c"..orange.."Grylls|rExpBar usage:")
-        DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb move |r - toggle allowing moving of the bar by dragging it")   
+        DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb move |r - toggle allowing moving of the bar by dragging it")
         DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb width n|r - set bar to width n")
         DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb height n|r - set bar to height n")
         DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb border |r - toggle bar border")
         DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb dark |r - toggle dark theme border")
-        DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb class |r - toggle class coloring of exp bar")  
-        DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb reset |r - reset bar to default settings")             
+        DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb class |r - toggle class coloring of exp bar")
+        DEFAULT_CHAT_FRAME:AddMessage("|c"..yellow.."/geb reset |r - reset bar to default settings")
     end
 end
 
@@ -336,10 +336,8 @@ GryllsExpBar:SetScript("OnEvent", function()
             SLASH_GRYLLSEXPBAR1 = "/gryllsexpbar"
             SLASH_GRYLLSEXPBAR2 = "/geb"
             SlashCmdList["GRYLLSEXPBAR"] = GryllsExpBar_commands
-
 			createBar()
             setBar()
-
 			DEFAULT_CHAT_FRAME:AddMessage("|cffff8000Grylls|rGryllsExpBar loaded! /geb")
             GryllsExpBar.move = false
             GryllsExpBar.loaded = true
@@ -349,7 +347,7 @@ GryllsExpBar:SetScript("OnEvent", function()
         updateRep()
     elseif event == "PLAYER_XP_UPDATE" then
         updateExp()
-    elseif event == "UPDATE_FACTION" then   
+    elseif event == "UPDATE_FACTION" then
         updateRep()
     end
 end)
